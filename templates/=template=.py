a98b339879a7b592c8f50fa022784a00b31ccd5c
tinyproxy <tinyproxy@gmail.com>
2022-07-11 14:27:46 +0800

    update python3 template

--- templates/=template=.py
+++ templates/=template=.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python3
 import logging
-from argparse import ArgumentParser
-# import pandas
-# from pathlib import Path
 
 
 def main():
     logging.basicConfig(
         level=logging.INFO,
-        format='[%(asctime)s] %(filename)s:%(lineno)d %(message)s'                            # easy format
-        # format='{%(process)s:%{thread}d} [%(asctime)s] %(filename)s:%(lineno)d %(message)s' # power format
+        format='[%(asctime)s] %(filename)s:%(lineno)d %(message)s'
     )
-    cli_parser = ArgumentParser()
-    cli_parser.add_argument()
-    cli_configure = cli_parser.parse_args()
-    '''CODE CONTINUES HERE%HERE%'''
 
 
 if __name__ == '__main__':
