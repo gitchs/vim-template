b068ccd5ca0417f76a9e958fdbed5b56a51e544a
tinyproxy <tinyproxy@gmail.com>
2022-07-14 15:45:13 +0800

    update template

--- templates/=template=.sql
+++ templates/=template=.sql
@@ -1,22 +1,3 @@
-REFRESH default.workload_query;
-
--- DROP TABLE IF EXISTS default.test00;
-
--- CREATE TABLE IF NOT EXISTS default.test00
--- STORED AS PARQUET
--- LOCATION 'HDFS_LOCATION' -- CTAS should modify this HDFS_LOCATION
--- AS
-
-SELECT
-`day`
-,day_to_date(CAST(`day` AS INT)) AS `date`
-,COUNT(*) AS nquery
--- %HERE%
-FROM default.workload_query
-WHERE `day` BETWEEN 0 AND 1
-GROUP BY `day`
-ORDER BY `day`
-;
-
-
-
+-- CREATED AT %FDATE%
+-- AUTHOR %USER%
+%HERE%
